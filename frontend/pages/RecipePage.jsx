64cfdd259ca67fb574e99711244a6050b11d39b0
homerofsteel <lucas.v85@outlook.com>
2026-05-06 13:31:32 +0200

    add recipe ingredients

--- frontend/pages/RecipePage.jsx
+++ frontend/pages/RecipePage.jsx
@@ -5,12 +5,19 @@ import "./RecipePage.css";
 export default function RecipePage() {
   const { id } = useParams();
   const [recipe, setRecipe] = useState(null);
+  const [ingredients, setIngredients] = useState(null);
   const [liked, setLiked] = useState(false);
 
   useEffect(() => {
     fetch(`http://localhost:5000/api/recipes/${id}`)
       .then(res => res.json())
       .then(data => setRecipe(data));
+  }, [id]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/recipes/${id}/ingredients`)
+      .then(res => res.json())
+      .then(data => setIngredients(data));
   }, [id]);
 
   if (!recipe) return <p>Erreur lors du fetch de la recette</p>;
@@ -24,7 +31,7 @@ export default function RecipePage() {
 
       <article className="recipe">
         
-        <div className="recipe__header">
+        <header className="recipe__header">
           <h1>{recipe.name}</h1>
 
           <span
@@ -33,21 +40,32 @@ export default function RecipePage() {
             style={{ color: liked ? "red" : "gray" }}
           >
             favorite
-          </span>
-        </div>
+          </span> 
+        </header>
 
-        <div className="recipe__info">
+        <section className="recipe__info">
           <p>
             <strong>Pays :</strong> {recipe.country_name}
           </p>
-        </div>
+        </section>
 
         <img src={recipe.image_url} className="recipe__image"></img>
 
-        <div className="recipe__instructions">
+        <section className="recipe__ingredients">
+          <h3>Ingrédients</h3>
+          <ul>
+            {ingredients.map(ingredient => (
+              <li key={ingredient.id}>
+                {ingredient.name} — {ingredient.quantity} {ingredient.unit}
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <section className="recipe__instructions">
           <h3>Instructions</h3>
           <p>{recipe.instructions}</p>
-        </div>
+        </section>
 
       </article>
 
